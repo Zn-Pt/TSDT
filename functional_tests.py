77f7a8a64124f26b5d26a9b1d3b23959e9689b04
Zn_Pt <ZnPt@bupt.edu.cn>
2024-03-21 15:36:07 +0800

    message 1:first commit

---
+++ functional_tests.py
@@ -0,0 +1,32 @@
+# from selenium import webdriver
+#
+# browser=webdriver.Chrome()
+# browser.get('http://localhost:8000')
+#
+# assert 'Django' in browser.page_source
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+# 弹出网页有一个定位输入的网页
+# 他去看了这个网页的首页
+browser.get('http://localhost:8000')
+
+# 他注意到网页里包含“To-Do”这个词
+assert 'To-Do' in browser.title
+
+# 应用有一个输入代办事项的文本输入框
+# 他在文本输入框里输入了“Buy flowers"
+# 他按了回车键确定，并页面更新了
+# 待办事项表格中显示了“1: Buy flowers"
+# 页面中又显示了一个文本输入框，可以输入其他待办事项
+# 他输入了“Send a gift to Lisa"
+# 页面再次更新，他的清单中显示了这两个待办事项
+# 弹出三栏知道这个网站是否会记住他的清单
+# 他看到网站为他生成了一个唯一的URL
+# 他访问那个URL，发现他的待办事项列表还在
+# 他满意的离开了
+
+browser.quit()
+
