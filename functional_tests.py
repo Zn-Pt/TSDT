551af44ccffe2280ce1d3b0fb77bd759bdca4de8
Zn_Pt <ZnPt@bupt.edu.cn>
2024-03-26 01:13:24 +0800

    message 2:The first functional_test

--- functional_tests.py
+++ functional_tests.py
@@ -1,32 +1,30 @@
-# from selenium import webdriver
-#
-# browser=webdriver.Chrome()
-# browser.get('http://localhost:8000')
-#
-# assert 'Django' in browser.page_source
-
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
+class NewVisitorTest(unittest.TestCase):
 
-# 弹出网页有一个定位输入的网页
-# 他去看了这个网页的首页
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-# 他注意到网页里包含“To-Do”这个词
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# 应用有一个输入代办事项的文本输入框
-# 他在文本输入框里输入了“Buy flowers"
-# 他按了回车键确定，并页面更新了
-# 待办事项表格中显示了“1: Buy flowers"
-# 页面中又显示了一个文本输入框，可以输入其他待办事项
-# 他输入了“Send a gift to Lisa"
-# 页面再次更新，他的清单中显示了这两个待办事项
-# 弹出三栏知道这个网站是否会记住他的清单
-# 他看到网站为他生成了一个唯一的URL
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # 张三听说有一个很棒的办事列表的应用
+        # 他去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 他注意到浏览器的标题包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title, "browser title was:" + self.browser.title)
+        self.fail('Finish the test!')
+
+        # 应用邀请他输入一个办事列表的文本框
+
+        # 他在文本框输入了“Buy flowers"
+
 # 他访问那个URL，发现他的待办事项列表还在
-# 他满意的离开了
+# 他感到非常满足
 
-browser.quit()
-
+if __name__ == '__main__':
+    unittest.main()
