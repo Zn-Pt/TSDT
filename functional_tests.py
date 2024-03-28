068a1e45bd83a9c928d223a33ae7bd44700696f2
Zn_Pt <ZnPt@bupt.edu.cn>
2024-03-28 15:44:55 +0800

    message 6: Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -10,21 +13,36 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-        # 张三听说有一个很棒的办事列表的应用
-        # 他去看了这个应用的首页
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        # 他注意到浏览器的标题包含“To-Do”这个词
-        self.assertIn('To-Do', self.browser.title, "browser title was:" + self.browser.title)
+        # 她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  # (1)
+        self.assertEqual('To-Do', header_text)
+
+        # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')  # (1)
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 她在文本框中输入了“Buy flowers”
+        inputbox.send_keys('Buy Flowers')  # (2)
+
+        # 她按回车键后，页面更新了
+        # 待办事项表格中显示了“1: Buy Flowers”
+        inputbox.send_keys(Keys.ENTER)  # (3)
+        time.sleep(1)  # (4)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')  # (1)
+        self.assertIn('1: Buy Flowers', [row.text for row in rows])
+
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # 她输入了“gift to girlfriend”
         self.fail('Finish the test!')
 
-        # 应用邀请他输入一个办事列表的文本框
-
-        # 他在文本框输入了“Buy flowers"
-
-# 他访问那个URL，发现他的待办事项列表还在
-# 他感到非常满足
-
-if __name__ == '__main__':
-    unittest.main()
+        # 页面再次更新，她的清单中显示了这两个待办事项
